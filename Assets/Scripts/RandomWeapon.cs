a942989cf5a7a54398c4e1b80ad173dba7b2d465
emeblast <eeckhouseeric@gmail.com>
2018-08-26 17:21:53 -0700

    Created a test script
    Created a test script to see if my functions are working

--- Assets/Scripts/RandomWeapon.cs
+++ Assets/Scripts/RandomWeapon.cs
@@ -10,64 +10,54 @@ public class RandomWeapon : MonoBehaviour
 	bool taken = false;
 	float t = 0;
      public GameObject prefadWeaponPickUp = GameObject.FindGameObjectWithTag("randomWeapon");
-
+    bool shit = false;
 	void Start()
 	{
 		mRender = GetComponent<MeshRenderer> ();
 	}
 
 
-	void Update()
-	{
-
-		//transform.position = new Vector3 (transform.position.x, Mathf.PingPong (Time.deltaTime, 2.0f), transform.position.z);
-
-       
-        //respawn if actor has been destroy, respawn item, with same position 
-        // get position, set position.
-		if(taken)
-		{
-			t += Time.deltaTime;
-			if(t >= waitTime)
-			{
-
-              
-                taken = false;
-				GetComponent<NetworkView>().RPC("setEnable",RPCMode.AllBuffered,true);
-
-			}
-            
-		}
-    }
+	
 
     void OnTriggerEnter(Collider other)
 	{
 		Debug.Log ("Player enter collider");//collider works
 		if(other.tag == "Player")//if collides with the mesh tagged player
 		{
+            
+
 			if(taken == false)
 			{
                 print("has been taken");
                 taken = true;
 				t = 0;
 				int wpn = RandomPrefab();
-                
-				//respawn game object
+
+                shit = true;
+                print(shit);
+				
 				// get world postion
 				GameObject weaponGenetor = Resources.Load("Prefab/randomWeapon")as GameObject;
 				Vector3 weaponLocation = transform.position;
                 Destroy(gameObject);
 
-
+               
                 //is respawning needs detla time, give time in between
                 t+=Time.deltaTime;
+                // print(t);
+                
                 if (t >= waitTime)
                 {
+
+                    print("helllllll no");
+                    //probably move this line of code inside of the update function
                     Instantiate(weaponGenetor, transform.position, transform.rotation);
 
 
                     //need to create sprite animation, with a GUI Layout
                     GameObject prefab = WeaponPrefabs[wpn];
+
+
                     Shooting Shoot = other.GetComponentInParent<Shooting>();
                     Shoot.Secondary = prefab;
                     //need to destroy game object
@@ -79,8 +69,37 @@ public class RandomWeapon : MonoBehaviour
 			}
 		}
 	}
+    void FixedUpdate()
+    {
 
-	int RandomPrefab()
+        //transform.position = new Vector3 (transform.position.x, Mathf.PingPong (Time.deltaTime, 2.0f), transform.position.z);
+
+
+        //respawn if actor has been destroy, respawn item, with same position 
+        // get position, set position.
+        if (shit == true)
+        {
+
+            print("its true");
+            
+        }
+
+        if (taken)
+        {
+            t += Time.deltaTime;
+            print("heeeeee");
+            if (t >= waitTime)
+            {
+
+
+                taken = false;
+                GetComponent<NetworkView>().RPC("setEnable", RPCMode.AllBuffered, true);
+
+            }
+
+        }
+    }
+    int RandomPrefab()
 	{
 		int ran = Random.Range (0, WeaponPrefabs.Length);
 		return ran;
---
+++ Assets/Scripts/TestWorkingVar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWorkingVar : MonoBehaviour {
+
+    bool coffee = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        print(coffee);
+
+        if(coffee)
+        {
+            print("it's dark coffee");
+
+        }
+
+	}
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            coffee = true;
+
+
+        }
+    }
+}
